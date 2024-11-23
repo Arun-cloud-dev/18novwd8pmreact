2ccd85efbe64f1c889f664389e7ef3f8342d940a
Arun raj <riderarunraj@live.com>
2024-11-23 16:20:21 +0530

    react update

---
+++ 02-starting-project/src/App.jsx
@@ -0,0 +1,35 @@
+
+
+
+
+
+function Header(){
+  return(
+  <header>
+    <img src="./assets/react-core-concepts.png" alt="Stylish logo" />
+    <h1>React Essentials</h1>
+    <p>Fundemental react Concepts You will need for Almost any app you going to Build </p>
+  </header>
+  )
+}
+
+
+
+
+
+
+
+function App(){
+  return(
+    <div>
+      <Header/>
+      <main>
+        <h2>Time to Get started </h2>
+      </main>
+      
+    </div>
+  );
+}
+
+
+export default App; 
