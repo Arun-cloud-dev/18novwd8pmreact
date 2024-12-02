8389d9046f0af3b1f281663e25cc129cd7513eca
Arun raj <riderarunraj@live.com>
2024-12-02 20:03:00 +0530

    concepts

--- 02-starting-project/src/App.jsx
+++ 02-starting-project/src/App.jsx
@@ -1,39 +1,35 @@
-import reactImg from './assets/react-core-concepts.png'
+// import reactImg from './assets/react-core-concepts.png'
+import  {react} from "react"
 import { CORE_CONCEPTS } from './data';
+import Header from "./Header"
+import CoreConcept from './CoreConcepts';
 
 
 
 
-const reactDescription = ['Fundemental' , 'Crucial', 'Core']
 
-function genRandomInt(max){
-  return Math.floor(Math.random()* (max+2));
-}
-
-
-
-function Header(){
-  const description = reactDescription[genRandomInt(2)]
-  return(
-  <header>
-    <img src={reactImg} alt="Stylish logo" />
-    <h1>React Essentials</h1>
-    <p>{description} react Concepts You will need for Almost any app you going to Build </p>
-  </header>
-  ) 
-}
+// function Header(){
+//   const description = reactDescription[genRandomInt(2)]
+//   return(
+//   <header>
+//     <img src={reactImg} alt="Stylish logo" />
+//     <h1>React Essentials</h1>
+//     <p>{description} react Concepts You will need for Almost any app you going to Build </p>
+//   </header>
+//   ) 
+// }
 
 
 
-function CoreConcept({image , title , description}){
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
+// function CoreConcept({image , title , description}){
+//   return (
+//     <li>
+//       <img src={image} alt={title} />
+//       <h3>{title}</h3>
+//       <p>{description}</p>
+//     </li>
+//   )
+// }
 
 
 function App(){
---
+++ 02-starting-project/src/Header.jsx
@@ -0,0 +1,22 @@
+
+import reactImg from './assets/react-core-concepts.png'
+
+
+const reactDescription = ['Fundemental' , 'Crucial', 'Core']
+
+function genRandomInt(max){
+  return Math.floor(Math.random()* (max+2));
+}
+
+
+
+export default function Header(){
+    const description = reactDescription[genRandomInt(2)]
+    return(
+    <header>
+      <img src={reactImg} alt="Stylish logo" />
+      <h1>React Essentials</h1>
+      <p>{description} react Concepts You will need for Almost any app you going to Build </p>
+    </header>
+    ) 
+  }
