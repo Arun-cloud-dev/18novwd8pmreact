959f0773ba0bbf09279856cecc814646e3e74670
Arun raj <riderarunraj@live.com>
2024-12-12 20:45:06 +0530

    update state login.jsx

--- 03-forms/src/components/StateLogin.jsx
+++ 03-forms/src/components/StateLogin.jsx
@@ -1,91 +1,95 @@
-import React, { useState } from "react";
-
-
-
-
+import { useState } from 'react';
 
 export default function Login() {
-    // const [enteredEmail, setEnteredEmail] =  useState('');
-    // const [enteredPassword, setenteredPassword] = useState('');
-    
-
-const [enteredValues , setEnteredValues] = useState({
-    email :'',
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
     password: '',
+  });
+
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(enteredValues);
+  }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
+
+
+
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
+
     
-})
+  }
 
-const emailIsInvalid = !enteredValues.email.includes('@')
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
+    }));
+  }
 
+  // function handleEmailChange(event) {
+  //   setEnteredEmail(event.target.value);
+  // }
 
+  // function handlePasswordChange(event) {
+  //   setEnteredPassword(event.target.value);
+  // }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        console.log(enteredValues)
-        setEnteredValues({
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
 
-        })
-
-    }
-
-    function handleInputChange(identifirer ,value){
-        setEnteredValues(prevvalues => ({
-            ...prevvalues, 
-            [identifirer]: value 
-        }))
-    }
-
-    // function handleEmailChange(event){
-    //     setEnteredEmail(event.target.value);
-
-    // }
-    // function handlepasswordChange(event ){
-    //     setenteredPassword(event.target.value);
-    // }
-
-
-  return(
-    
-    <form onSubmit={handleSubmit} >
-        <h2>Login</h2>
-
-        <div className="control-row">
-            <div className="control no-margin">
-                <label htmlFor="email">Email</label>
-                <input 
-                id="email" 
-                type="email" 
-                name="email" 
-                onChange={(event) => (handleInputChange('email',event.target.value))}
-                value={enteredValues.email}
-                />
-                <div className="control-error">
-                    {emailIsInvalid && <p>Please enter the valid email address </p>}
-                </div>
-            </div>
-
-            <div className="control no-margin">
-                <label htmlFor="password">Password</label>
-                <input 
-                id="password" 
-                type="password" 
-                name="password" 
-                onChange={(event) => (handleInputChange('password',event.target.value))}
-                value={enteredValues.password}
-                />
-            </div>
-           
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onBlur={() => handleInputBlur('email')}
+            onChange={(event) => handleInputChange('email', event.target.value)}
+            value={enteredValues.email}
+          />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
-        <p className="form-actions">
-            <button className="button button-flat">
-                Reset
-            </button>
-            <button className="button" onClick={handleSubmit}>
-                Login
-            </button>
-        </p>
-        
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) =>
+              handleInputChange('password', event.target.value)
+            }
+            value={enteredValues.password}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
     </form>
   );
 }
--- regex.js
+++ regex.js
@@ -1,15 +1,19 @@
-function validateGmail (email){
-    const gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+// function validateGmail (email){
+//     const gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
 
 
-    if (gmailRegex.test(email)){
-        console.log("valid Gmail address");
-    }else {
-        console.log('invalid Gmail address' );
-    }
-}
+//     if (gmailRegex.test(email)){
+//         console.log("valid Gmail address");
+//     }else {
+//         console.log('invalid Gmail address' );
+//     }
+// }
 
 
-validateGmail("ecample@ar.com")
-validateGmail('arunraj@gmailcom')
-validateGmail('arunraj@gmail.com')
+// validateGmail("ecample@ar.com")
+// validateGmail('arunraj@gmailcom')
+// validateGmail('arunraj@gmail.com')
+
+
+
+
